13c62053c7892b538e15f1d91eae7a43d10ed8a2
HP <HP@HP-PC>
2018-06-23 07:47:37 +0530

    New test case

--- Scripts/Amazonshopping/Script1529716492655.groovy
+++ Scripts/Amazonshopping/Script1529716492655.groovy
@@ -19,3 +19,10 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKe
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
+WebUI.openBrowser()
+
+WebUI.navigateToUrl('www.amazon.in'
+	
+WebUI.closeBrowser()
+}
+
